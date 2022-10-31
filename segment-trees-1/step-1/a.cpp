e0a917a5e42fbac8a9d6154ad2624fba24e745ba
Venom-61 <subhendukarmakar61@gmail.com>
2022-10-31 23:49:42 +0530

    Segment Tree for the Sum

--- segment-trees-1/step-1/a.cpp
+++ segment-trees-1/step-1/a.cpp
@@ -1,93 +1,91 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Segment Tree starts here ...
+// Segment tree for the sum
 
 struct segTree {
+
     int size;
-    vector<int> values;
+    vector<long long> sums;
+
+    int NEUTRAL = 0;
 
     // Initialize the Segment Tree
 
     void init(int n) {
         size = 1;
-        while(size < n)
-            size *= 2;
-        values.assign(2*size, 0LL);
+        while(size < n) size *= 2;
+        sums.assign(2*size, 0LL);
     }
 
-    // Build the Segment tree from the array
+    // Build the Segment Tree from an array
     
     void build(vector<int> &a, int x, int lx, int rx) {
         if(rx - lx == 1) {
             if(lx < (int)a.size()) {
-                values[x] = a[lx];
+                sums[x] = a[lx];
             }
             return ;
         }
-
+        
         int m = (lx + rx) / 2;
         build(a, 2*x + 1, lx, m);
         build(a, 2*x + 2, m, rx);
 
-        values[x] = min(values[2*x + 1], values[2*x + 2]);
+        sums[x] = sums[2*x + 1] + sums[2*x + 2];
     }
 
     void build(vector<int> &a) {
         build(a, 0, 0, size);
     }
 
-    // Assign value to the Segment Tree
-    
-    void set(int i, int v, int x, int lx, int rx) {
+    // Set the value to index in Segment Tree
+
+    void set(int i,int v, int x, int lx, int rx) {
         if(rx - lx == 1) {
-            values[x] = v;
+            sums[x] = v;
             return ;
         }
-
+         
         int m = (lx + rx) / 2;
-        if(i < m) {
+        if(i < m)
             set(i, v, 2*x + 1, lx, m);
-        } else {
+        else 
             set(i, v, 2*x + 2, m, rx);
-        }
 
-        values[x] = min(values[2*x + 1], values[2*x + 2]);
+        sums[x] = sums[2*x + 1] + sums[2*x + 2];
     }
-
+    
     void set(int i, int v) {
         set(i, v, 0, 0, size);
     }
 
-    // Calculate Minimum
+    // Calculate the sum int Segment Tree
 
-    int calc(int l, int r, int x, int lx, int rx) {
-        if(lx >= r or rx <= l) return INT_MAX;
-        if(lx >= l and rx <= r) return values[x];
-
+    long long sum(int l, int r, int x, int lx, int rx) {
+        if(lx >= r or l >= rx) return NEUTRAL;
+        if(lx >= l and rx <= r) return sums[x];
+        
         int m = (lx + rx) / 2;
-        long long s1 = calc(l, r, 2*x + 1, lx, m);
-        long long s2 = calc(l, r, 2*x + 2, m, rx);
-    
-        return min(s1, s2);
+        long long s1 = sum(l, r, 2*x + 1, lx, m);
+        long long s2 = sum(l, r, 2*x + 2, m, rx);
+        return s1 + s2; 
     }
 
-    int calc(int l, int r) {
-        return calc(l, r, 0, 0, size);
+    long long sum(int l, int r) {
+        return sum(l, r, 0, 0, size);
     }
 
 };
-
-// Segment Tree ends here ....
 
 int main() {
     ios::sync_with_stdio(false);
     
     int n, q;
     cin >> n >> q;
-
     vector<int> a(n);
-    for(auto &x : a) cin >> x;
+    for(auto &x : a) 
+        cin >> x;
 
     segTree st;
     st.init(n);
@@ -103,9 +101,11 @@ int main() {
         } else {
             int l, r;
             cin >> l >> r;
-            cout << st.calc(l, r) << "\n";
+            cout << st.sum(l, r) << "\n";
         }
     }
 
+
     return 0;
 }
+
